7d443b325e285a055744ce77f00bf6e519aac3a4
Owen Thibodeau <139512947+owenjtb@users.noreply.github.com>
2026-09-14 08:09:27 -0400

    Add files via upload

---
+++ Assignment2A.java
@@ -0,0 +1,7 @@
+public class Assignment2A {
+    public static void main(String[] args) {
+        System.out.println("My name is Owen.\r\n" + //
+                        "I am in grade 11.\r\n" + //
+                        "My favourite activity is video games.");
+    }
+}
