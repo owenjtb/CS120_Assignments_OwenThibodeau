017ec3d9cc4bdc7543e7d4e8b2d98704f452d70a
Owen Thibodeau <139512947+owenjtb@users.noreply.github.com>
2026-09-15 09:17:09 -0300

    Add files via upload

---
+++ Assignment2c.java
@@ -0,0 +1,12 @@
+class Assignment2C {
+    
+    public static void main (String[ ] args) 
+    {
+        double number = 127.51927;
+        System.out.printf("One decimal      %.1f\n" +
+            "Two decimals     %.2f\n" +
+            "Three decimals   %.3f\n" +
+            "Four decimals    %.4f\n" +
+            "Five decimals    %.5f\n", number, number, number, number, number);
+    }
+}
