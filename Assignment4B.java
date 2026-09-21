12d0019e9ac76f8d76dc16fd7950daec602ac5bf
Owen Thibodeau <139512947+owenjtb@users.noreply.github.com>
2026-09-21 09:06:26 -0400

    Add files via upload

---
+++ Assignment4B.java
@@ -0,0 +1,10 @@
+public class Assignment4B {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
+            
+        }
+        System.out.print(sum);
+    }
+}
