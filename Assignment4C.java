56608d2ef6565a273f3e99a29ec63d82eec0e691
Owen Thibodeau <139512947+owenjtb@users.noreply.github.com>
2026-09-22 08:14:18 -0400

    Add files via upload

---
+++ Assignment4C.java
@@ -0,0 +1,8 @@
+public class Assignment4C {
+        public static void main(String[] args) {
+            
+            for (int i = 50; i <= 70; i++) {
+                System.out.printf("%.5f \n",Math.sqrt(i));
+                }
+            }
+}
